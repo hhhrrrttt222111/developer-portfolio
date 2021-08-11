db35f806283d3c9811923d8e0057af753d1c6df0
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-08-11 15:57:41 +0530

    😶

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,8 +1,8 @@
 import { // eslint-disable-next-line
-    theme1, theme4, theme5, theme6, theme7, theme8, theme9, theme10, theme11, theme12, theme13, theme14, theme15
+    theme1, theme2, theme3, theme4, theme5, theme6, theme7, theme8, theme9, theme10, theme11, theme12, theme13, theme14, theme15
 } from '../theme/theme'  
 
 
 export const themeData = {
-    theme: theme15
+    theme: theme10
 }
