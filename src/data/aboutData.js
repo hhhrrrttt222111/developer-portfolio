aba8a38355e547dff94765de780bef3aced9b082
Hemanth R <hemanthrt18@gmail.com>
2021-08-14 14:14:43 +0530

    removed warning

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I’m the lead developer at a local agency. During the evenings I spend my time working on freelance projects and building my own products.During the evenings I spend my time working on freelance projects and building my own products."
+    description2: "During the day I’m the lead developer at a local agency. During the evenings I spend my time working on freelance projects and building my own products. During the evenings I spend my time working on freelance projects and building my own products. During the day I’m the lead developer at a local agency. During the evenings I spend my time working on freelance projects and building my own products"
 }
