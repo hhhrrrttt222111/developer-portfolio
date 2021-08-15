c82e48ed1d7e2471f220c861a1088acc4e7b8d36
Hemanth R <hemanthrt18@gmail.com>
2021-08-15 20:04:25 +0530

    images file

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,19 +1,8 @@
-import blueThemegirl from '../assets/svg/about/blueTheme-girl.svg'
-import blueThemeboy from '../assets/svg/about/blueTheme-boy.svg'
-import bwThemeboy from '../assets/svg/about/bwTheme-boy.svg'
-import bwThemegirl from '../assets/svg/about/bwTheme-girl.svg'
-import greenThemeboy from '../assets/svg/about/greenTheme-boy.svg'
-import greenThemegirl from '../assets/svg/about/greenTheme-girl.svg'
-import orangeThemeboy from '../assets/svg/about/orangeTheme-boy.svg'
-import orangeThemegirl from '../assets/svg/about/orangeTheme-girl.svg'
-import pinkThemeboy from '../assets/svg/about/pinkTheme-boy.svg'
-import pinkThemegirl from '../assets/svg/about/pinkTheme-girl.svg'
-import purpleThemeboy from '../assets/svg/about/purpleTheme-boy.svg'
-import purpleThemegirl from '../assets/svg/about/purpleTheme-girl.svg'
-import redThemeboy from '../assets/svg/about/redTheme-boy.svg'
-import redThemegirl from '../assets/svg/about/redTheme-girl.svg'
-import yellowThemeboy from '../assets/svg/about/yellowTheme-boy.svg'
-import yellowThemegirl from '../assets/svg/about/yellowTheme-girl.svg'
+import { 
+    blueThemegirl, blueThemeboy, bwThemeboy, bwThemegirl, greenThemeboy, greenThemegirl, orangeThemeboy, 
+    orangeThemegirl, pinkThemeboy, pinkThemegirl, purpleThemeboy, purpleThemegirl, redThemeboy, redThemegirl, yellowThemeboy,
+    yellowThemegirl
+} from './images'
 
 
 export const greenThemeLight = {
