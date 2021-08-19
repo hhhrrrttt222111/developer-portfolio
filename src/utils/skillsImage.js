91fb8da24ba61f5caab1cf50c11ec0a3a7aaefeb
Hemanth R <hemanthrt18@gmail.com>
2021-08-19 18:45:55 +0530

    skills

--- src/utils/skillsImage.js
+++ src/utils/skillsImage.js
@@ -5,6 +5,27 @@ import illustrator from '../assets/svg/skills/illustrator.svg'
 import docker from '../assets/svg/skills/docker.svg'
 import adobeXd from '../assets/svg/skills/adobe-xd.svg'
 import afterEffects from '../assets/svg/skills/after-effects.svg'
+import css from '../assets/svg/skills/css.svg'
+import angular from '../assets/svg/skills/angular.svg'
+import javascript from '../assets/svg/skills/javascript.svg'
+import nextJS from '../assets/svg/skills/nextJS.svg'
+import nuxtJS from '../assets/svg/skills/nuxtJS.svg'
+import react from '../assets/svg/skills/react.svg'
+import svelte from '../assets/svg/skills/svelte.svg'
+import typescript from '../assets/svg/skills/typescript.svg'
+import vue from '../assets/svg/skills/vue.svg'
+
+import bootstrap from '../assets/svg/skills/bootstrap.svg'
+import bulma from '../assets/svg/skills/bulma.svg'
+import capacitorjs from '../assets/svg/skills/capacitorjs.svg'
+import coffeescript from '../assets/svg/skills/coffeescript.svg'
+import memsql from '../assets/svg/skills/memsql.svg'
+import mongoDB from '../assets/svg/skills/mongoDB.svg'
+import mysql from '../assets/svg/skills/mysql.svg'
+import postgresql from '../assets/svg/skills/postgresql.svg'
+import tailwind from '../assets/svg/skills/tailwind.svg'
+import vitejs from '../assets/svg/skills/vitejs.svg'
+import vuetifyjs from '../assets/svg/skills/vuetifyjs.svg'
 
 export const skillsImage = (skill) => {
     const skillID = skill.toLowerCase();
@@ -23,6 +44,46 @@ export const skillsImage = (skill) => {
             return adobeXd;
         case 'after effects':
             return afterEffects;
+        case 'css':
+            return css;
+        case 'angular':
+            return angular;
+        case 'javascript':
+            return javascript;
+        case 'next js':
+            return nextJS;
+        case 'nuxt js':
+            return nuxtJS;
+        case 'react':
+            return react;
+        case 'svelte':
+            return svelte;
+        case 'typescript':
+            return typescript;
+        case 'vue':
+            return vue;
+        case 'bootstrap':
+            return bootstrap;
+        case 'bulma':
+            return bulma;
+        case 'capacitorjs':
+            return capacitorjs;
+        case 'coffeescript':
+            return coffeescript;
+        case 'memsql':
+            return memsql;
+        case 'mongodb':
+            return mongoDB;
+        case 'mysql':
+            return mysql;
+        case 'postgresql':
+            return postgresql;
+        case 'tailwind':
+            return tailwind;
+        case 'vitejs':
+            return vitejs;
+        case 'vuetifyjs':
+            return vuetifyjs;
         default:
             break;
     }
