1b711c485a96b56d5a79224ba8b73e42eb9fe6b1
Hemanth R <hemanthrt18@gmail.com>
2021-08-27 13:34:27 +0530

    contactsData

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react';
 
+import { ThemeContext } from '../../contexts/ThemeContext';
+
+
+import { contactsData } from '../../data/contactsData'
 import './Contacts.css'
 
 function Contacts() {
+
+    const { theme } = useContext(ThemeContext);
+    
     return (
         <div className="contacts" id="contacts">
             <h1>Contacts</h1>
