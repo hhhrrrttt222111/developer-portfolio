a95e0635fc258107d3d39b445b9e8a5f73f7d3c0
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-08-28 12:32:27 +0530

    services

---
+++ src/data/servicesData.js
@@ -0,0 +1,34 @@
+import { BiShoppingBag } from "react-icons/bi";
+import { BiPencil } from "react-icons/bi";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaInternetExplorer } from "react-icons/fa";
+import { FaChalkboardTeacher } from "react-icons/fa";
+ 
+
+export const servicesData = [
+    {
+        id: 1,
+        title: 'Marketing',
+        icon: <BiShoppingBag/>
+    },
+    {
+        id: 2,
+        title: 'Content Writing',
+        icon: <BiPencil /> 
+    },
+    {
+        id: 3,
+        title: 'Email Marketing',
+        icon: <AiOutlineMail />
+    },
+    {
+        id: 4,
+        title: 'Internet Research',
+        icon: <FaInternetExplorer />
+    },
+    {
+        id: 5,
+        title: 'Tutoring',
+        icon: <FaChalkboardTeacher />
+    }
+]
