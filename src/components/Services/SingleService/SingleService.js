f690ed44c06e1047f51fb3eab0b1539a82775e6a
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-08-28 12:32:27 +0530

    services

--- src/components/Services/SingleService/SingleService.js
+++ src/components/Services/SingleService/SingleService.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useContext} from 'react';
 
-function SingleService() {
+import { ThemeContext } from '../../../contexts/ThemeContext';
+
+import './SingleService.css'
+
+
+function SingleService({id, title, icon}) {
+
+    const { theme } = useContext(ThemeContext);
     return (
-        <div>
-            
+        <div key={id} className="single-service" style={{backgroundColor:theme.primary}}>
+           <div className="service-content">
+               <i className="service-icon">{icon}</i>
+              <h4>{title}</h4>  
+            </div>         
         </div>
     )
 }
