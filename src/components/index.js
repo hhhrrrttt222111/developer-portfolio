2cfc88f34eb972a3dea0255b616f766fce0df8e4
Hemanth R <hemanthrt18@gmail.com>
2021-08-28 15:56:50 +0530

    back to top

--- src/components/index.js
+++ src/components/index.js
@@ -13,3 +13,5 @@ export { default as Projects } from'./Projects/Projects'
 export { default as Services } from'./Services/Services'
 export { default as SingleProject } from './Projects/SingleProject/SingleProject'
 export { default as SingleService } from './Services/SingleService/SingleService'
+
+export { default as BackToTop } from'./BackToTop/BackToTop'
