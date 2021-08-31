a1352e43cfcb790f2fe606cd03dfdd2ff57adeee
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-08-31 13:12:23 +0530

    contacts bakki

--- src/components/Services/SingleService/SingleService.js
+++ src/components/Services/SingleService/SingleService.js
@@ -12,9 +12,9 @@ function SingleService({id, title, icon}) {
     return (
         <Fade bottom>
             <div key={id} className="single-service" style={{backgroundColor:theme.primary400}}>
-                <div className="service-content">
+                <div className="service-content"  style={{color:theme.tertiary}}>
                     <i className="service-icon">{icon}</i>
-                    <h4>{title}</h4>  
+                    <h4  style={{color:theme.tertiary}}>{title}</h4>  
                 </div>         
             </div>
         </Fade>
