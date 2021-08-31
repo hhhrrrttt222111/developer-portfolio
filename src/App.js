7b7e4b9ae75882b635961f41247f56cd3cf6bb54
Hemanth R <hemanthrt18@gmail.com>
2021-08-31 11:08:49 +0530

    console msg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { ThemeContext } from './contexts/ThemeContext';
 import { Main, BlogPage, ProjectPage } from './pages'
 import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
@@ -8,6 +9,12 @@ import ScrollToTop from './utils/ScrollToTop'
 import './App.css'
 
 function App() {
+
+  const { theme } = useContext(ThemeContext);
+
+  console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
+  console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
+  console.log = console.warn = console.error = () => {};
 
   return (
     <div className="app">
