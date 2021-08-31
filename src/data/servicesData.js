eb3d6192ebabe6e758bee0134fba14f8a2826f27
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-08-31 09:23:35 +0530

    serviceData

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,8 +1,8 @@
-import { BiShoppingBag } from "react-icons/bi";
-import { BiPencil } from "react-icons/bi";
-import { AiOutlineMail } from "react-icons/ai";
-import { FaInternetExplorer } from "react-icons/fa";
-import { FaChalkboardTeacher } from "react-icons/fa";
+import { BiShoppingBag, BiPencil } from "react-icons/bi";
+import { BsCodeSlash, BsClipboardData } from "react-icons/bs";
+import { AiOutlineMail, AiFillAudio } from "react-icons/ai";
+import { FaInternetExplorer,  FaChalkboardTeacher, FaCameraRetro, FaPinterest, FaVideo, FaTabletAlt, FaRegNewspaper } from "react-icons/fa";
+
  
 
 export const servicesData = [
@@ -35,5 +35,42 @@ export const servicesData = [
     {
         id: 6,
         title: 'Web Design',
-        icon: */
+        icon: <BsCodeSlash />
+    },
+    {
+        id: 7,
+        title: 'Photography',
+        icon: <FaCameraRetro />
+    },
+    {
+        id: 8,
+        title: 'Pinterest Virtual Assistant',
+        icon: <FaPinterest />
+    }, 
+    {
+        id: 9,
+        title: 'Video Editing',
+        icon: <FaVideo />
+    },
+    {
+        id: 10,
+        title: 'Data Entry', 
+        icon: <BsClipboardData />
+    },
+    {
+        id: 11,
+        title: 'Ebook Writing',
+        icon: <FaTabletAlt />
+    },
+    {
+        id: 12,
+        title: 'Audio Transcription',
+        icon: <AiFillAudio />
+    },
+    {
+        id: 13,
+        title: 'Blog Posting',
+        icon: <FaRegNewspaper />
+    },
+   */
 ]
