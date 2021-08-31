d41064e5cdcd95d4c457eb0be53c31060e2984ea
Hemanth R <hemanthrt18@gmail.com>
2021-08-31 09:22:31 +0530

    service mobile view

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
-  <React.StrictMode>
     <ThemeContextProvider>
       <App />
-    </ThemeContextProvider>
-  </React.StrictMode>,
+    </ThemeContextProvider>,
   document.getElementById('root')
 );
 
