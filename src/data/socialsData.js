29ff922c1c7a6e3759611e50827114d1dfb441ad
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-09-03 20:02:34 +0530

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -3,7 +3,7 @@ export const socialsData = {
     facebook: 'https://www.facebook.com/shimil.abraham.18',
     linkedIn: 'https://www.linkedin.com/in/shimil-abraham-47049a1b6/',
     instagram: 'https://www.instagram.com/_shimil._.abraham/',
-    codepen: 'https://codepen.io/',
+    codepen: 'https://codepen.io/shimilsabraham',
     youtube: 'https://www.youtube.com/channel/UC5n4ot4dA-AZmPUy6cORrqA',
     // twitter: 'https://twitter.com/',
     // reddit: 'https://www.reddit.com/user/',
