a3d2d137b76d4e52cda4b3c88cad5df8bf5aaebe
Hemanth R <hemanthrt18@gmail.com>
2021-09-03 17:06:56 +0530

    minor updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,13 @@ import { headerData } from '../../data/headerData'
 
 function Footer() {
 
+    const shortname = (name) => {
+        if(name.length > 10) {
+            return name.split(" ")[0]
+        } else {
+            return name
+        }
+    }
 
     const { theme }  = useContext(ThemeContext)
 
@@ -15,7 +22,7 @@ function Footer() {
                 <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
                     ❤
                 </span>
-                 by {headerData.name}
+                 by {shortname(headerData.name)}
             </p>
         </div>
     )
