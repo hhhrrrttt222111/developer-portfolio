5c20319f87ae8ddce00b9c380bd566441c0858d5
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-09-05 15:05:19 +0530

    achievement

--- src/components/Achievement/AchievementCard.js
+++ src/components/Achievement/AchievementCard.js
@@ -8,7 +8,7 @@ import { AiOutlineFolder } from "react-icons/ai";
 
 import './Achievement.css'
 
-function AchievementCard({id, description, details, date, field, image}) {
+function AchievementCard({id, title, details, date, field, image}) {
 
     const { theme } = useContext(ThemeContext);
 
@@ -27,7 +27,7 @@ function AchievementCard({id, description, details, date, field, image}) {
            <div key={id} className={`achievement-card ${classes.achievementCard}`}>
                <div className="achievecard-content">
                     <div className="achievecard-details1">
-                        <h2 style={{color: theme.tertiary}}>{description}</h2>
+                        <h2 style={{color: theme.tertiary}}>{title}</h2>
                         <p style={{color: theme.secondary}}>{details}</p>
                     </div>
                     <div className="achievecard-details2" style={{color: theme.tertiary80}}>
