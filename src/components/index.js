0789175bab54a79a6aa1fc9b05bbb9ad25c936f4
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-09-05 14:13:47 +0530

    achievement

--- src/components/index.js
+++ src/components/index.js
@@ -13,5 +13,6 @@ export { default as Projects } from'./Projects/Projects'
 export { default as Services } from'./Services/Services'
 export { default as SingleProject } from './Projects/SingleProject/SingleProject'
 export { default as SingleService } from './Services/SingleService/SingleService'
+export { default as Achievement } from './Achievement/Achievement'
 
 export { default as BackToTop } from'./BackToTop/BackToTop'
---
+++ src/data/achievementData.js
@@ -0,0 +1,30 @@
+export const achievementData = {
+    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    achievements : [
+        {
+            id : 1,
+            description : 'My transition from a Software Engineer to a CS grad student',
+            details : 'Why I decided to go for a Master’s degree',
+            date : 'Aug 20, 2019',
+            field : 'Automation',
+            image : ''
+        },
+        {
+            id : 2,
+            description : 'My transition from a Software Engineer to a CS grad student',
+            details : 'Why I decided to go for a Master’s degree',
+            date : 'Aug 20, 2019',
+            field : 'Automation',
+            image : ''
+        },
+        {
+            id : 3,
+            description : 'My transition from a Software Engineer to a CS grad student',
+            details : 'Why I decided to go for a Master’s degree',
+            date : 'Aug 20, 2019',
+            field : 'Automation',
+            image : ''
+        }
+
+    ]
+}
