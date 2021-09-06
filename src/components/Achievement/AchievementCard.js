360813cb8af401eccd5019832e46472ef5f07dba
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-09-06 11:16:01 +0530

    achievement

--- src/components/Achievement/AchievementCard.js
+++ src/components/Achievement/AchievementCard.js
@@ -30,7 +30,7 @@ function AchievementCard({id, title, details, date, field, image}) {
                         <h2 style={{color: theme.tertiary}}>{title}</h2>
                         <p style={{color: theme.secondary}}>{details}</p>
                     </div>
-                    <div className="achievecard-details2" style={{color: theme.tertiary80}}>
+                    <div className="achievecard-details2" style={{color: theme.primary}}>
                         <h5>{date}</h5>
                         <div className="achievecard-field">
                             <AiOutlineFolder />
