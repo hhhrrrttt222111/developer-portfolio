d6a5c9fc51027df2a3f3d7bb8e95fd0765e3e8ab
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-09-13 12:32:02 +0530

    achievement

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -3,27 +3,27 @@ export const achievementData = {
     achievements : [
         {
             id : 1,
-            title : 'My transition from a Software Engineer to a CS grad student',
-            details : 'Why I decided to go for a Master’s degree Why I decided to go for a Master’s degree',
+            title : 'Google Cloud certification',
+            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
             date : 'Aug 20, 2019',
             field : 'Automation',
-            image : ''
+            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
         },
         {
             id : 2,
-            title : 'My transition from a Software Engineer to a CS grad student',
-            details : 'Why I decided to go for a Master’s degree',
+            title : 'AWS Certification',
+            details : 'Validate technical skills and cloud expertise to grow your career and business.',
             date : 'Aug 20, 2019',
             field : 'Automation',
-            image : ''
+            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
         },
         {
             id : 3,
-            title : 'My transition from a Software Engineer to a CS grad student',
-            details : 'Why I decided to go for a Master’s degree',
+            title : 'Professional Data Engineer Certification',
+            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
             date : 'Aug 20, 2019',
             field : 'Automation',
-            image : ''
+            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
         }
 
     ]
