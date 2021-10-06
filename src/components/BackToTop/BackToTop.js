c9a2a1fb8313816269405fbdb6954e7c4f5e2bfc
Senior-DevStar <jhonatanmaranosilva@gmail.com>
2021-10-06 15:12:37 -0400

    Add aria labels

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -1,53 +1,49 @@
-import React, { useState, useContext } from 'react'
-import { IoIosArrowDropupCircle } from "react-icons/io";
+import React, { useState, useContext } from 'react';
+import { IoIosArrowDropupCircle } from 'react-icons/io';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { ThemeContext } from '../../contexts/ThemeContext';
-import './BackToTop.css'
+import './BackToTop.css';
 
 function BackToTop() {
+  const [visible, setVisible] = useState(false);
 
-    const [visible, setVisible] = useState(false)
+  const { theme } = useContext(ThemeContext);
 
-    const { theme } = useContext(ThemeContext);
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
 
-    const toggleVisible = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300){
-            setVisible(true)
-        } 
-        else if (scrolled <= 300){
-            setVisible(false)
-        }
-    };
-      
-    const scrollToTop = () =>{
-        window.scrollTo({
-            top: 0, 
-            behavior: 'smooth'
-        });
-    };
-      
-    window.addEventListener('scroll', toggleVisible);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
+  window.addEventListener('scroll', toggleVisible);
 
-    const useStyles = makeStyles(() => ({
-        icon: {
-            fontSize: '3rem',
-            color: theme.tertiary,
-        }
-    }));
+  const useStyles = makeStyles(() => ({
+    icon: {
+      fontSize: '3rem',
+      color: theme.tertiary,
+    },
+  }));
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-
-    return (
-        <div style={{display: visible ? 'inline' : 'none'}} className="backToTop">
-            <button onClick={scrollToTop}>
-                <IoIosArrowDropupCircle className={classes.icon}/>
-            </button>
-        </div>
-    )
+  return (
+    <div style={{ display: visible ? 'inline' : 'none' }} className='backToTop'>
+      <button onClick={scrollToTop} aria-label='Back to top'>
+        <IoIosArrowDropupCircle className={classes.icon} />
+      </button>
+    </div>
+  );
 }
 
-export default BackToTop
+export default BackToTop;
