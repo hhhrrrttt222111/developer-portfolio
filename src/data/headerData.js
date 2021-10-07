e8dba2bf3f59000299a85e6ffc2445451dea4e42
ladcva <ladcva@yahoo.com>
2021-10-07 22:11:36 +0700

    Initial commit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import profilePic from '../assets/jpg/244404849_406836764407075_8235459219945703560_n.jpg'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
+    name: 'Hoang Thi Ngoc Thu',
+    title: "Content Marketer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: profilePic,
     resumePdf: resume
 }
