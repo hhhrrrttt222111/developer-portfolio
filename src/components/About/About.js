558552875e50ddf871a5dacca3258665c5a88203
Hana <fejzic.hana@gmail.com>
2021-10-25 20:10:13 +0200

    About me fixed and added text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,8 +19,9 @@ function About() {
             <div className="about-body">
                 <div className="about-description">
                     <h2 style={{color: theme.primary}}>{aboutData.title}</h2>
-                    <p style={{color:theme.tertiary80}}>{aboutData.description1}<br/><br/>{aboutData.description2}</p>
+                    <p style={{color:theme.tertiary80}}>{aboutData.description1}<br/><br/>{aboutData.description2}<br/><br/>{aboutData.description3}<br/>{aboutData.description4}</p>
                 </div>
+                <div className="about-imgcontainer">
                 <div className="about-img1">
                     <img 
                         //src={aboutData.image === 1 ? theme.aboutimg1 : theme.aboutimg2}  
@@ -34,6 +35,7 @@ function About() {
                         src="https://scontent-vie1-1.xx.fbcdn.net/v/t1.18169-9/18921855_1668790259822167_2405359986941678364_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=nIcYGpyL-B4AX-nVRUI&_nc_ht=scontent-vie1-1.xx&oh=6e4ab533f320ee690516f5c8a61ab8f6&oe=6199A50C"
                         alt="" 
                     />
+                </div>
                 </div>
             </div>
         </div>
--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name's Hana. I'm a software engineering student based in Bosnia and Herzegovina.",
+    description2: "When I want to take a break from studying I turn to nature. That's where photography comes in. I created Druid's Photos because I wanted to combine my love for art and nature. I think my country has many beautiful and underrated landscapes that I want to share with the world. I hope you enjoy my work.",
+    description3:"Equipment: Canon EOS 4000D ",
+    description4:"Editing software: Adobe Lightroom",
     image: 2
 }
