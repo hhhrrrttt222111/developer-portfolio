286393fe0b4516698022cc7734923a46264fb656
Anindya-Kundu <64122347+Anindya-Kundu@users.noreply.github.com>
2022-07-22 23:51:13 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "Hi my name's Anindya Kundu. I'm a Data Scientist based in India.",
+    description2: "A seasoned Data Scientist with 12 years of industry experience. Currently working as a Lead Data Scientist in a multi-national techno-consulting firm in the area of financial risk and fraud detection. I  work on different research topics in my free time and utilize my time to built my own products based on my area of interest. I spent my leisure hours writing articles. Right now I'm learning graph database and it’s utilization in fraud detection. I love to learn, travel and explore new arenas.",
     image: 2
 }
