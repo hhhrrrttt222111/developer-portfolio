c862a0c2a50e78c398f37fc5d342fd5d6066af81
jatinvyas997 <65624987+jatinvyas997@users.noreply.github.com>
2022-10-09 23:04:34 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name's Jatin vyas. I'm a Senior Technical Associate and developer based in Delhi, India.",
+    description2: "During the day I work as a developer at a local agency and in the evening I utilize the time to built my own skills. I spent my leisure hours reading articles and books,and I love to learn and explore new arenas.",
     image: 2
 }
