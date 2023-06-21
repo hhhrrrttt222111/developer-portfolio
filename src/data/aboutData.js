66111a835a752d57ac1c3496d2560a7e4c7e4f49
Thabo1995 <mailathabo1@gmail.com>
2023-06-21 18:23:53 +0200

    profilesummary

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Bokang alvina ntsoane. I'm a Human Resource Manager based in Centurion, RSA.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description2: "Highly skilled and accomplished Human Resource Manager with 6 years of experience in effectively managing and enhancing HR functions within diverse organizations. Proven track record of implementing strategic HR initiatives, fostering a positive work environment, and driving employee engagement. Strong expertise in talent acquisition, employee relations, performance management, and policy development. Committed to optimizing HR operations to support organizational goals and promoting a culture of inclusivity and excellence.",
     image: 2
 }
