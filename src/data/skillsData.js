22014b742eccebca32f01c902ac7b2e2d77af314
datawrangler <vinodkumar9576@gmail.com>
2023-08-14 15:57:13 +0530

    Update skillsData.js

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -11,7 +11,7 @@
     'Pytorch',
     'Tensorflow',
     'Microsoft Office',
-    'Matlab',
+    'GCP',
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
