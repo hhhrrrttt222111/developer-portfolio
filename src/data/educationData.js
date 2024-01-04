ca3e7410400cd1f76d9b8b7dd74bb423909543de
BenLalor <lalor.benjamin@gmail.com>
2024-01-04 13:52:42 -0700

    Customize educationData

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,16 @@
 export const educationData = [
-    {
-        id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
-    },
-    {
-        id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
-    },
-    {
-        id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
-    },
-]
+  {
+    id: 1,
+    institution: "Colorado State University",
+    course: "B.S. Computer Science",
+    startYear: "2020",
+    endYear: "2023",
+  },
+  {
+    id: 2,
+    institution: "Sonoma State University",
+    course: "B.S. Energy Management and Design",
+    startYear: "2010",
+    endYear: "2015",
+  },
+];
