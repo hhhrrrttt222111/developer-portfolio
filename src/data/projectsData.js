67c392bbf1ca24cdfd1ecc63d132915f1ee00564
BenLalor <lalor.benjamin@gmail.com>
2024-01-04 17:33:56 -0700

    Customize projectData and associated images

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,88 +1,39 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-
+import CalculatorImage from "../assets/svg/projects/CalculatorImage.svg";
+import StockerImage from "../assets/svg/projects/StockerImage.svg";
+import LTSImage from "../assets/svg/projects/LTSImage.svg";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
-    },
-    {
-        id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
-    },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
-]
-
+  {
+    id: 1,
+    projectName: "Calculator",
+    projectDesc:
+      "This project is a calculator that allows users to perform basic arithmetic operations.",
+    tags: ["React", "CSS", "JSX"],
+    code: "https://github.com/BenLalor/Calculator_React",
+    demo: "https://benlalor.github.io/Calculator_React/",
+    image: CalculatorImage,
+  },
+  {
+    id: 2,
+    projectName: "Stock Data Visualizer",
+    projectDesc:
+      "This project allows a user to search for a stock and view historical and current financial data, visualized in a graph.",
+    tags: ["React", "CSS", "Alpha Vantage API"],
+    code: "https://github.com/BenLalor/Stock_Visualization_React",
+    demo: "https://benlalor.github.io/Stock_Visualization_React/",
+    image: StockerImage,
+  },
+  {
+    id: 3,
+    projectName: "Energy Code Form Completion Tool",
+    projectDesc:
+      "Full stack web application that allows users to register, login, logout, complete the NRCC-LTS Energy Code Form, revisit and revise previously existing forms.",
+    tags: ["Flask", "Vanilla Javascript", "CSS", "HTML"],
+    code: "https://github.com/BenLalor/LTS_ComplianceSoftware_VanillaJavascript",
+    demo: "https://github.com/BenLalor/LTS_ComplianceSoftware_VanillaJavascript",
+    image: LTSImage,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
