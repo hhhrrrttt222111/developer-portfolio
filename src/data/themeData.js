6d9e41bb53663c4bd85284a6831fe6958b986942
BenLalor <lalor.benjamin@gmail.com>
2024-01-05 15:29:07 -0700

    Customize image color

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,12 +1,26 @@
 /* eslint-disable */
-import { 
-    greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
-} from '../theme/theme'
-
+import {
+  greenThemeLight,
+  greenThemeDark,
+  bwThemeLight,
+  bwThemeDark,
+  blueThemeLight,
+  blueThemeDark,
+  redThemeLight,
+  redThemeDark,
+  orangeThemeLight,
+  orangeThemeDark,
+  purpleThemeLight,
+  purpleThemeDark,
+  pinkThemeLight,
+  pinkThemeDark,
+  yellowThemeLight,
+  yellowThemeDark,
+  armyGreenThemeDark,
+} from "../theme/theme";
 
 export const themeData = {
-    theme: orangeThemeDark
-}
-
+  theme: armyGreenThemeDark,
+};
 
 // Choose theme from above
