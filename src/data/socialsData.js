7adf1c27c80c358e78f1d4d8a2eb2855a0a65ff8
BenLalor <lalor.benjamin@gmail.com>
2024-01-05 15:31:50 -0700

    Update social links and customize services icons

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,5 +1,5 @@
 export const socialsData = {
   github: "https://github.com/BenLalor",
-  linkedIn: "https://www.linkedin.com/in",
+  linkedIn: "https://www.linkedin.com/in/ben-lalor-1aab8997/",
   stackOverflow: "https://stackoverflow.com/users/23198138/benlalor",
 };
