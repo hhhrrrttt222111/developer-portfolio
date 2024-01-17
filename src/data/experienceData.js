760eb77ea10c943c5f97ac5546816b0b9ee33583
BenLalor <lalor.benjamin@gmail.com>
2024-01-17 12:44:33 -0700

    reorder experienceData

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,16 +1,16 @@
 export const experienceData = [
   {
     id: 1,
-    company: "EnergySoft",
-    jobtitle: "Senior Nonresidential Energy Analyst",
-    startYear: "2014",
-    endYear: "2019",
-  },
-  {
-    id: 2,
     company: "NORESCO",
     jobtitle: "Energy Analyst III",
     startYear: "2019",
     endYear: "Present",
+  },
+  {
+    id: 2,
+    company: "EnergySoft",
+    jobtitle: "Senior Nonresidential Energy Analyst",
+    startYear: "2014",
+    endYear: "2019",
   },
 ];
