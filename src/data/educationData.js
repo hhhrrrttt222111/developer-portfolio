85aa4a36fee98630a9dd274990bf7ecf1eb129ad
BenLalor <lalor.benjamin@gmail.com>
2024-01-17 12:39:33 -0700

    Clean up description and remove ed dates

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -3,14 +3,14 @@ export const educationData = [
     id: 1,
     institution: "Colorado State University",
     course: "B.S. Computer Science",
-    startYear: "2020",
-    endYear: "2023",
+    startYear: "",
+    endYear: "",
   },
   {
     id: 2,
     institution: "Sonoma State University",
     course: "B.S. Energy Management and Design",
-    startYear: "2010",
-    endYear: "2015",
+    startYear: "",
+    endYear: "",
   },
 ];
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,9 +3,9 @@ import profilePic from "../assets/png/profilepic.jpeg";
 
 export const headerData = {
   name: "Ben Lalor",
-  title: "Full Stack Web Developer",
+  title: "Full Stack Developer",
   desciption:
-    "My name's Ben Lalor. I'm a full stack web developer based in Denver, CO with a background in building energy efficiency and building energy codes. I have a passion for automation and creating clean, intuitive, and dynamic user experiences.",
+    "My name's Ben Lalor. I'm a full stack developer based in Denver, CO with a background in building energy efficiency and building energy codes. I have a passion for automation and creating clean, intuitive, and dynamic user experiences.",
   image: profilePic,
   resumePdf: BenLalor_Resume,
 };
